c3dbe646afc6b0480d7fb60e6a59d24ace3c6754
Ion Lefter <ionlefter1997@gmail.com>
2024-10-16 10:49:28 +0300

    144. Two-Way Binding: Repetition

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -15,11 +15,15 @@ export class NewTicketComponent implements OnInit, AfterViewInit{
   @ViewChild('form') form?: ElementRef<HTMLFormElement>;
   //private form = viewChild.required <ElementRef<HTMLFormElement>>('form');
   //@Output() add = new EventEmitter<{title: string; text: string}>();
+  enteredTitle = '';
+  enteredText = '';
   add = output<{title: string; text: string}>();
 
-  onSubmit(title: string, ticketText: string){
-    this.add.emit({title: title, text: ticketText})
-    this.form?.nativeElement.reset();
+  onSubmit(){
+    this.add.emit({title: this.enteredTitle, text: this.enteredText})
+    //this.form?.nativeElement.reset();
+    this.enteredTitle = '';
+    this.enteredText = '';
   }
   ngOnInit(): void {
     console.log('ONINIT');
